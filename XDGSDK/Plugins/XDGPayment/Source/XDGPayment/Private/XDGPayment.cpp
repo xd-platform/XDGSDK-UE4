c88e19863ef35669b2dbe539a68404ec9d9f027c
guiming <suguiming@xd.com>
2021-09-26 19:38:50 +0800

    add plugins

---
+++ XDGSDK/Plugins/XDGPayment/Source/XDGPayment/Private/XDGPayment.cpp
@@ -0,0 +1,53 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XDGPayment.h"
+#include "Core.h"
+#include "Modules/ModuleManager.h"
+#include "Interfaces/IPluginManager.h"
+#include "XDGPaymentLibrary/ExampleLibrary.h"
+
+#define LOCTEXT_NAMESPACE "FXDGPaymentModule"
+
+void FXDGPaymentModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	// Get the base directory of this plugin
+	FString BaseDir = IPluginManager::Get().FindPlugin("XDGPayment")->GetBaseDir();
+
+	// Add on the relative location of the third party dll and load it
+	FString LibraryPath;
+#if PLATFORM_WINDOWS
+	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/XDGPaymentLibrary/Win64/ExampleLibrary.dll"));
+#elif PLATFORM_MAC
+    LibraryPath = FPaths::Combine(*BaseDir, TEXT("Source/ThirdParty/XDGPaymentLibrary/Mac/Release/libExampleLibrary.dylib"));
+#elif PLATFORM_LINUX
+	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/XDGPaymentLibrary/Linux/x86_64-unknown-linux-gnu/libExampleLibrary.so"));
+#endif // PLATFORM_WINDOWS
+
+	ExampleLibraryHandle = !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
+
+	if (ExampleLibraryHandle)
+	{
+		// Call the test function in the third party library that opens a message box
+		ExampleLibraryFunction();
+	}
+	else
+	{
+		FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("ThirdPartyLibraryError", "Failed to load example third party library"));
+	}
+}
+
+void FXDGPaymentModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+
+	// Free the dll handle
+	FPlatformProcess::FreeDllHandle(ExampleLibraryHandle);
+	ExampleLibraryHandle = nullptr;
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FXDGPaymentModule, XDGPayment)
---
+++ XDGSDK/Source/XDGSDK/XDGBlueprintLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "XDGBlueprintLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XDGSDK_API UXDGBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
